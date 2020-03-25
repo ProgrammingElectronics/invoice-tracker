31d79847cc5316f46ce160abfc8ecb1edfca0cbc
mcheich <mcheich@gmail.com>
2020-03-25 16:47:02 -0400

    initial commit

---
+++ src/test/java/com/invoicetracker/InvoiceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoicetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
