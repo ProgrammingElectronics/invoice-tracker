82a164549bcd14934b2001a74ecca0affc10caec
mcheich <mcheich@gmail.com>
2020-03-25 20:33:15 -0400

    added abstract user class

---
+++ src/main/java/com/invoicetracker/models/User.java
@@ -0,0 +1,104 @@
+package com.invoicetracker.models;
+
+public abstract class User {
+
+	
+	/************************ Field Values****************/
+	
+	/* Contact Info */
+	private String firstName;
+	private String lastName;
+	private String businessName;
+	private String email;
+	
+	/* Address */
+	private String addressLineOne;
+	private String addressLineTwo;
+	private String city;
+	private String state;
+	private String zip;
+	private String country;
+	
+	/************************ Getters and Setters ****************/
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getBusinessName() {
+		return businessName;
+	}
+	
+	public void setBusinessName(String businessName) {
+		this.businessName = businessName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getAddressLineOne() {
+		return addressLineOne;
+	}
+	
+	public void setAddressLineOne(String addressLineOne) {
+		this.addressLineOne = addressLineOne;
+	}
+	
+	public String getAddressLineTwo() {
+		return addressLineTwo;
+	}
+	
+	public void setAddressLineTwo(String addressLineTwo) {
+		this.addressLineTwo = addressLineTwo;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getState() {
+		return state;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public String getZip() {
+		return zip;
+	}
+	
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+}
