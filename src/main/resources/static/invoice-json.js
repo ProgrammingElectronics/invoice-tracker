f551384a418474f122cadaf41c88416f3c3296fc
Caleb-Stripes <calebdstripes@gmail.com>
2020-04-01 17:23:50 -0400

    closing in on the JSON file

--- src/main/resources/static/invoice-json.js
+++ src/main/resources/static/invoice-json.js
@@ -1,19 +1,72 @@
+var invoiceArray = [];
+
 function test() {
+
     var serviceInfoSets = document.getElementsByClassName('serviceInfoSet');
     for (let infoSet of serviceInfoSets) {
-        console.log(infoSet.id);
+        // console.log(infoSet.id);
+        var innerInvoiceId = infoSet.id;
+        var clientName;
+        var serviceName;
+        var serviceDate;
+        var serviceTime;
+        var hourlyRate;
+        var amountDue;
+
+
         var values = infoSet.getElementsByClassName('info');
         for (let value of values) {
-            console.log(value.value);
+            tempValue = value.value;
+            if (value.id == 'clientName') {
+                clientName = tempValue;
+                // console.log('test1');
+            }
+            if (value.id == 'serviceName') {
+                serviceName = tempValue;
+                // console.log('test2');
+            }
+            if (value.id == 'serviceDate') {
+                serviceDate = tempValue;
+                // console.log('test3');
+            }
+            if (value.id == 'serviceTime') {
+                serviceTime = tempValue;
+                // console.log('test4');
+            }
+            if (value.id == 'hourlyRate') {
+                hourlyRate = tempValue;
+                console.log('test5');
+            }
+            if (value.id == 'amountDue') {
+                amountDue = tempValue;
+                // console.log('test6');
+            }
         }
+
+        console.log(innerInvoiceId, clientName, serviceName, serviceDate, serviceTime, hourlyRate, amountDue);
+
+        var infoSetValues = {
+            'innerInvoiceId': innerInvoiceId,
+            'clientName': clientName,
+            'serviceName': serviceName,
+            'serviceDate': serviceDate,
+            'serviceTime': serviceTime,
+            'hourlyRate': hourlyRate,
+            'amountDue': amountDue
+        }
+
+        // console.log(infoSetValues);
+        invoiceArray.push(infoSetValues);
+
     }
 
 }
 
+console.log(invoiceArray);
 
 // **** this function of a for/of loop allows iteration of a html collection ****
 // **** this example also prints to the console each DOM's id                ****
-// var list = document.getElementsByClassName("events");
+// var list = document.getElementsByClassName('events');
 // for (let item of list) {
 //     console.log(item.id);
 // }
