86928f929912a7fdd95514f96da05b095d5f17fb
mcheich <mcheich@gmail.com>
2020-04-10 09:29:38 -0400

    All JPA test for relationships green again

--- src/test/java/com/invoicetracker/JPAMappingsTest.java
+++ src/test/java/com/invoicetracker/JPAMappingsTest.java
@@ -76,138 +76,141 @@ public class JPAMappingsTest {
 		assertEquals(contractor.getFirstName(), "firstName");
 	}
 
-//	@Test
-//	public void shouldGenrateContractorId() {
-//		// Arrange
-//		Contractor contractor = contractorRepo.save(new Contractor("firstName"));
-//		long contractorId = contractor.getId();
-//
-//		// Act
-//		entityManager.flush();
-//		entityManager.clear();
-//
-//		Optional<Contractor> result = contractorRepo.findById(contractorId);
-//		contractor = result.get();
-//
-//		// Assert
-//		assertThat(contractor.getId(), is(greaterThan(0L)));
-//	}
-//
-//	
-//	@Test
-//	public void shouldSaveAndLoadInvoiceDateOfService() {
-//		// Arrange
-//		LocalDate date = LocalDate.of(2020, 03, 28);
-//		Invoice invoice = invoiceRepo.save(new Invoice(date));
-//		Long invoiceId = invoice.getId();
-//
-//		// Act
-//		entityManager.flush();
-//		entityManager.clear();
-//
-//		Optional<Invoice> result = invoiceRepo.findById(invoiceId);
-//		invoice = result.get();
-//
-//		// Assert
-//		assertEquals(invoice.getDateOfInvoice(), date);
-//	}
-//
-//	@Test
-//	public void shouldGenerateInvoiceId() {
-//		// Arrange
-//		LocalDate date = LocalDate.of(2020, 03, 28);
-//		Invoice invoice = invoiceRepo.save(new Invoice(date));
-//		Long invoiceId = invoice.getId();
-//
-//		// Act
-//		entityManager.flush();
-//		entityManager.clear();
-//
-//		Optional<Invoice> result = invoiceRepo.findById(invoiceId);
-//		invoice = result.get();
-//
-//		// Assert
-//		assertThat(invoice.getId(), is(greaterThan(0L)));
-//	}
-//
-//	@Test
-//	public void shouldGenerateServiceItemId() {
-//		// Arrange
-//		LocalDate date = LocalDate.of(2020, 03, 28);
-//		ServiceItem serviceItem = serviceItemRepo.save(new ServiceItem(date));
-//		long serviceItemId = serviceItem.getId();
-//
-//		// Act
-//		// Act
-//		entityManager.flush();
-//		entityManager.clear();
-//
-//		Optional<ServiceItem> result = serviceItemRepo.findById(serviceItemId);
-//		serviceItem = result.get();
-//
-//		// Assert
-//		assertThat(serviceItem.getId(), is(greaterThan(0L)));
-//	}
-//
-//	@Test
-//	public void shouldSaveAndLoadServiceItemDateOfService() {
-//		// Arrange
-//		LocalDate date = LocalDate.of(2020, 03, 28);
-//		ServiceItem serviceItem = serviceItemRepo.save(new ServiceItem(date));
-//		long serviceItemId = serviceItem.getId();
-//
-//		// Act
-//		entityManager.flush();
-//		entityManager.clear();
-//
-//		Optional<ServiceItem> result = serviceItemRepo.findById(serviceItemId);
-//		serviceItem = result.get();
-//
-//		// Assert
-//		assertEquals(serviceItem.getDateOfService(), date);
-//	}
-//
-//	@Test
-//	public void shouldEstablishInvoiceToServiceItemRelationship() {
-//		// Arrange
-//		LocalDate dateServiceOne = LocalDate.of(2020, 03, 28);
-//		LocalDate dateServiceTwo = LocalDate.of(2020, 03, 29);
-//		ServiceItem serviceOne = serviceItemRepo.save(new ServiceItem(dateServiceOne));
-//		ServiceItem serviceTwo = serviceItemRepo.save(new ServiceItem(dateServiceTwo));
-//		LocalDate dateOfInvoice = LocalDate.of(2020, 03, 29);
-//		//Invoice invoice = invoiceRepo.save(new Invoice(dateOfInvoice, serviceOne, serviceTwo));
-//		Invoice invoice = invoiceRepo.save(new Invoice(dateOfInvoice));
-//		invoice.addServiceItem(serviceOne);
-//		invoice.addServiceItem(serviceTwo);
-//		long invoiceId = invoice.getId();
-//
-//		// Act
-//		entityManager.flush();
-//		entityManager.clear();
-//
-//		Optional<Invoice> result = invoiceRepo.findById(invoiceId);
-//		invoice = result.get();
-//
-//		// Assert
-//		assertThat(invoice.getServiceItems(), containsInAnyOrder(serviceOne, serviceTwo));
-//	}
-//
-//	@Test
-//	public void shouldEstablishContractorToInvoiceRelationship() {
-//		// Arrange
-//		Invoice invoiceOne = invoiceRepo.save(new Invoice());
-//		Invoice invoiceTwo = invoiceRepo.save(new Invoice());
-//		Contractor contractor = contractorRepo.save(new Contractor(invoiceOne, invoiceTwo));
-//		long contractorId = contractor.getId();
-//
-//		// Act
-//		entityManager.flush();
-//		entityManager.clear();
-//
-//		Optional<Contractor> result = contractorRepo.findById(contractorId);
-//		contractor = result.get();
-//
-//		assertThat(contractor.getInvoices(), containsInAnyOrder(invoiceOne, invoiceTwo));
-//	}
+	@Test
+	public void shouldGenrateContractorId() {
+		// Arrange
+		Contractor contractor = contractorRepo.save(new Contractor("firstName"));
+		long contractorId = contractor.getId();
+
+		// Act
+		entityManager.flush();
+		entityManager.clear();
+
+		Optional<Contractor> result = contractorRepo.findById(contractorId);
+		contractor = result.get();
+
+		// Assert
+		assertThat(contractor.getId(), is(greaterThan(0L)));
+	}
+
+	
+	@Test
+	public void shouldSaveAndLoadInvoiceDateOfService() {
+		// Arrange
+	    Contractor contractor = contractorRepo.save(new Contractor());
+		Invoice invoice = invoiceRepo.save(new Invoice(contractor));
+		
+		LocalDate date = LocalDate.of(2020, 03, 28);
+		invoice.setDateOfInvoice(date);
+		Long invoiceId = invoice.getId();
+
+		// Act
+		entityManager.flush();
+		entityManager.clear();
+
+		Optional<Invoice> result = invoiceRepo.findById(invoiceId);
+		invoice = result.get();
+
+		// Assert
+		assertEquals(invoice.getDateOfInvoice(), date);
+	}
+
+	@Test
+	public void shouldGenerateInvoiceId() {
+		// Arrange
+	    Contractor contractor = contractorRepo.save(new Contractor());
+		Invoice invoice = invoiceRepo.save(new Invoice(contractor));
+		Long invoiceId = invoice.getId();
+
+		// Act
+		entityManager.flush();
+		entityManager.clear();
+
+		Optional<Invoice> result = invoiceRepo.findById(invoiceId);
+		invoice = result.get();
+
+		// Assert
+		assertThat(invoice.getId(), is(greaterThan(0L)));
+	}
+
+	@Test
+	public void shouldGenerateServiceItemId() {
+		// Arrange
+		Contractor contractor = contractorRepo.save(new Contractor());
+		Invoice invoice = invoiceRepo.save(new Invoice(contractor));
+		ServiceItem serviceItem = serviceItemRepo.save(new ServiceItem(invoice));
+		long serviceItemId = serviceItem.getId();
+
+		// Act
+		// Act
+		entityManager.flush();
+		entityManager.clear();
+
+		Optional<ServiceItem> result = serviceItemRepo.findById(serviceItemId);
+		serviceItem = result.get();
+
+		// Assert
+		assertThat(serviceItem.getId(), is(greaterThan(0L)));
+	}
+
+	@Test
+	public void shouldSaveAndLoadServiceItemDateOfService() {
+		// Arrange
+		Contractor contractor = contractorRepo.save(new Contractor());
+		Invoice invoice = invoiceRepo.save(new Invoice(contractor));
+		ServiceItem serviceItem = serviceItemRepo.save(new ServiceItem(invoice));
+		LocalDate date = LocalDate.of(2020, 03, 28);
+		serviceItem.setDateOfService(date);
+		long serviceItemId = serviceItem.getId();
+
+		// Act
+		entityManager.flush();
+		entityManager.clear();
+
+		Optional<ServiceItem> result = serviceItemRepo.findById(serviceItemId);
+		serviceItem = result.get();
+
+		// Assert
+		assertEquals(serviceItem.getDateOfService(), date);
+	}
+
+	@Test
+	public void shouldEstablishInvoiceToServiceItemRelationship() {
+		// Arrange
+		Contractor contractor = contractorRepo.save(new Contractor());
+		Invoice invoice = invoiceRepo.save(new Invoice(contractor));
+		ServiceItem serviceItemOne = serviceItemRepo.save(new ServiceItem(invoice));
+		ServiceItem serviceItemTwo = serviceItemRepo.save(new ServiceItem(invoice));
+		
+		long invoiceId = invoice.getId();
+
+		// Act
+		entityManager.flush();
+		entityManager.clear();
+
+		Optional<Invoice> result = invoiceRepo.findById(invoiceId);
+		invoice = result.get();
+
+		// Assert
+		assertThat(invoice.getServiceItems(), containsInAnyOrder(serviceItemOne, serviceItemTwo));
+	}
+
+	@Test
+	public void shouldEstablishContractorToInvoiceRelationship() {
+		// Arrange
+		Contractor contractor = contractorRepo.save(new Contractor());
+		Invoice invoiceOne = invoiceRepo.save(new Invoice(contractor));
+		Invoice invoiceTwo = invoiceRepo.save(new Invoice(contractor));
+		long contractorId = contractor.getId();
+
+		// Act
+		entityManager.flush();
+		entityManager.clear();
+
+		Optional<Contractor> result = contractorRepo.findById(contractorId);
+		contractor = result.get();
+
+		assertThat(contractor.getInvoices(), containsInAnyOrder(invoiceOne, invoiceTwo));
+	}
 
 }
